4ab515cbe497335cf594161fdc188ce73049a4eb
Asou8888 <szshenruizheng@126.com>
2018-07-05 20:54:31 +0800

    Update NewPage2.xaml.cs

--- MyWeatherApp-ToComplete/NewPage2.xaml.cs
+++ MyWeatherApp-ToComplete/NewPage2.xaml.cs
@@ -44,7 +44,35 @@ namespace MyWeatherApp
          */
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // to complete
+            Frame rootFrame = Window.Current.Content as Frame;
+      /*      if (rootFrame.CanGoBack)
+            {
+                // Show UI in title bar if opted-in and in-app backstack is not empty.
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    AppViewBackButtonVisibility.Visible;
+            }
+            else
+            {
+                // Remove the UI from the title bar if in-app back stack is empty.
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    AppViewBackButtonVisibility.Collapsed;
+            }*/
+
+            base.OnNavigatedTo(e);
+            if (e.NavigationMode == NavigationMode.New)
+            {
+                ApplicationData.Current.LocalSettings.Values.Remove("TheWorkInProgress");
+            }
+            else
+            {
+                if (ApplicationData.Current.LocalSettings.Values.ContainsKey("TheWorkInProgress"))
+                {
+                    var composite = ApplicationData.Current.LocalSettings.Values["TheWorkInProgress"] as ApplicationDataCompositeValue;
+                    nameBlock.Text = (string)composite["userName"];
+                    passwordBlock.Password = (string)composite["userPassword"];
+                    ApplicationData.Current.LocalSettings.Values.Remove("TheWorkInProgress");
+                }
+            }
         }
 
         /*
@@ -52,7 +80,16 @@ namespace MyWeatherApp
          */
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
-            // to complete
+            base.OnNavigatedFrom(e);
+
+            bool suspending = ((App)App.Current).isSuspended;
+            if (suspending)
+            {
+                var composite = new ApplicationDataCompositeValue();
+                composite["userName"] = nameBlock.Text;
+                composite["userPassword"] = passwordBlock.Password;
+                ApplicationData.Current.LocalSettings.Values["TheWorkInProgress"] = composite;
+            }
         }
 
         /*
